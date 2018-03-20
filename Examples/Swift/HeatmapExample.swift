f7780594f7e072485653348b6215ad1eaadaf514
jmkiley <jordan.kiley@mapbox.com>
2018-03-19 18:20:31 -0700

    converting the Swift

--- Examples/Swift/HeatmapExample.swift
+++ Examples/Swift/HeatmapExample.swift
@@ -34,36 +34,41 @@ class HeatmapExample: UIViewController, MGLMapViewDelegate {
         layer.heatmapColor = NSExpression.mgl_expression(forInterpolateFunction: .heatmapDensity, curveType: .linear, steps: colorDictionary)
         layer.heatmapIntensity = NSExpression.mgl_expression(forInterpolateFunction: .zoomLevel, curveType: .linear, steps: [0: 1, 9:3])
         layer.heatmapRadius = NSExpression.mgl_expression(forInterpolateFunction: .zoomLevel, curveType: .linear, steps: [0: 4, 9: 30])
-        layer.heatmapWeight = NSExpression.mgl_expression(forInterpolateFunction: .init(rawValue: "magnitude"), curveType: .linear, steps: [0: 0, 6: 1])
+        
+        layer.heatmapWeight = NSExpression.mgl_expression(forInterpolateFunction: NSExpression(forKeyPath: "mag"),
+                                                          curveType: .linear,
+                                                          parameters: nil,
+                                                          steps: NSExpression(forConstantValue:  [0: 0, 6: 1]))
+        layer.heatmapOpacity = NSExpression.mgl_expression(forStepFunction: .zoomLevel, defaultValue: 0.75 as NSValue, stops: [9: 0])
+        
 //        layer.heatmapColor = NSExpression(format: "FUNCTION($heatmapDensity, 'mgl_interpolateWithCurveType:parameters:stops:', 'linear', nil, %@)", colorDictionary)
 
 //        layer.heatmapIntensity = NSExpression(format: "FUNCTION($zoomLevel, 'mgl_interpolateWithCurveType:parameters:stops:', 'linear', nil, %@)",
 //                                              [0: 1,
 //                                               9: 3])
-        
 //        layer.heatmapRadius = NSExpression(format: "FUNCTION($zoomLevel, 'mgl_interpolateWithCurveType:parameters:stops:', 'linear', nil, %@)",
 //                                           [0: 4,
 //                                            9: 30])
-        layer.heatmapWeight = NSExpression(format: "FUNCTION(magnitude, 'mgl_interpolateWithCurveType:parameters:stops:', 'linear', nil, %@)",
-                                           [0: 0,
-                                            6: 1])
-        
-        layer.heatmapOpacity = NSExpression(format: "FUNCTION($zoomLevel, 'mgl_stepWithMinimum:stops:', 0.75, %@)", [9: 0])
+//        layer.heatmapWeight = NSExpression(format: "FUNCTION(magnitude, 'mgl_interpolateWithCurveType:parameters:stops:', 'linear', nil, %@)",
+//                                           [0: 0,
+//                                            6: 1])
+//        layer.heatmapOpacity = NSExpression(format: "FUNCTION($zoomLevel, 'mgl_stepWithMinimum:stops:', 0.75, %@)", [9: 0])
         
         style.addLayer(layer)
         
         // TODO: Add circle layer for higher zoom levels
-        let magnitudeDictionary : [Double : UIColor] = [0 : .white,
-                                                        2.5 : .yellow,
-                                                        5 : UIColor(red:0.73, green:0.23, blue:0.25, alpha:1.0),
-                                                        7.5 : UIColor(red:0.19, green:0.30, blue:0.80, alpha:1.0)
+        let magnitudeDictionary : [NSNumber : UIColor] = [0 : .white,
+                                                        0.5 : .yellow,
+                                                        2.5 : UIColor(red:0.73, green:0.23, blue:0.25, alpha:1.0),
+                                                        5 : UIColor(red:0.19, green:0.30, blue:0.80, alpha:1.0)
                                                         ]
         let circleLayer = MGLCircleStyleLayer(identifier: "circle-layer", source: source)
-        circleLayer.circleColor = NSExpression(format: "FUNCTION(mag, 'mgl_interpolateWithCurveType:parameters:stops:', 'linear', nil, %@)", magnitudeDictionary)
-        circleLayer.circleOpacity = NSExpression(format: "FUNCTION($zoomLevel, 'mgl_stepWithMinimum:stops:', 0, %@)", [9: 0.75])
+        circleLayer.circleColor = NSExpression.mgl_expression(forInterpolateFunction: NSExpression(forKeyPath: "mag"), curveType: .linear, parameters: nil, steps: NSExpression(forConstantValue: magnitudeDictionary))
+        circleLayer.circleOpacity = NSExpression.mgl_expression(forStepFunction: .zoomLevel, defaultValue: 0 as NSValue, stops: [9: 0.75])
         circleLayer.circleRadius = NSExpression(forConstantValue: 20)
+        circleLayer.circleStrokeColor = NSExpression(forConstantValue: UIColor.white)
+        circleLayer.circleStrokeWidth = NSExpression(forConstantValue: 6)
+        circleLayer.circleStrokeOpacity = NSExpression.mgl_expression(forStepFunction: .zoomLevel, defaultValue: 0 as NSValue, stops: [9: 0.75])
         style.addLayer(circleLayer)
-        
     }
-
 }
